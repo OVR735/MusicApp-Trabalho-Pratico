f8cefa5576ad3fbf0d7a58014a05dc232fab9c8a
amaryam12souza <amaryamlucas@gmail.com>
2024-06-03 21:37:04 -0300

    Criação da estrutura do projeto e da classe Usuario

---
+++ include/Usuario.h
@@ -0,0 +1,16 @@
+#ifndef USER_H_
+#define USER_H_
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Usuario {
+    public:
+    Usuario();
+
+    ~Usuario();
+ private:
+};
+
+#endif
---
+++ src/Usuario.cpp
@@ -0,0 +1,9 @@
+#include "../include/Usuario.h"
+
+Usuario::Usuario() {
+    cout << "Construtor do Usuario" << endl;
+}
+
+Usuario::~Usuario() {
+    cout << "Destrutor do Usuario" << endl;
+}
